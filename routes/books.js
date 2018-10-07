1e708c1885ec1749df03090f9d4318699a1cbacd
Warren Leyes <warren.leyes@gmail.com>
2018-10-06 18:01:28 -0700

    set basic routes which will need to be adjusted

--- routes/books.js
+++ routes/books.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index',  { title: 'All Books' });
 });
 
+/* GET new books form listing. */
+router.get('/new', function(req, res, next) {
+  res.render('new-book',  { title: 'New Book' });
+});
+
+/* POST new books form listing. */
+router.post('/new', function(req, res, next) {
+  res.render('new-book',  { title: 'New Book' });
+});
+
+/* GET books by id listing. */
+router.get('/:id', function(req, res, next) {
+  res.render('update-book',  { title: 'Update Book' });
+});
+
+/* POST books by id listing. */
+router.post('/:id', function(req, res, next) {
+  res.render('update-book',  { title: 'Update Book' });
+});
+
+/* POST books by id listing. */
+router.post('/:id/delete', function(req, res, next) {
+  res.render('update-book',  { title: 'Delete Book' });
+});
+
 module.exports = router;
