485eb29f45bb3f85ea126cb2b5f5a8c65ea2d07f
Warren Leyes <warren.leyes@gmail.com>
2018-10-07 22:19:09 -0700

    started error handling

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var booksRouter = require('./routes/books');
+const indexRouter = require('./routes/index');
+const booksRouter = require('./routes/books');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,16 +19,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// catch 404 and forward to error handler ::: <= ISSUE: works with invalid off main route but not off of invalid id  /books/:id
+app.use((req, res, next) =>{
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use( (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/books.js
+++ routes/books.js
@@ -3,51 +3,61 @@ const router = express.Router();
 const Book = require("../models").Book;
 
 /* GET books listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) =>{
   Book.findAll().then( books =>{
     res.render('index',  { title: 'All Books', books });
   });
 });
 
 /* GET new books form listing. */
-router.get('/new', function(req, res, next) {
-  res.render('new-book',  { title: 'New Book', book: Book.build() });
+router.get('/new', (req, res, next) => res.render('new-book',  { title: 'New Book', book: Book.build() }) );
+
+/* POST CREATE new books by id. */
+router.post('/new', (req, res, next) => {
+  Book.create(req.body)
+    .then( () => res.redirect(`/books`))
+    .catch(err =>{
+      if(err.name === "SequelizeValidationError"){
+        res.render('new-book',  {
+          title: 'New Book',
+          book: Book.build(req.body),
+          errors: err.errors
+        });
+      } else {
+        throw err;
+      }
+    })
+    .catch(error => res.sendStatus(500, error));
 });
 
-/* POST new books form listing. */
-router.post('/new', function(req, res, next) {
-  Book.create(req.body).then( book =>{
-    res.redirect(`/books/`);
-  });
+/* GET READ books by id. */
+router.get('/:id', (req, res, next) =>{
+  Book.findById(req.params.id).then( book =>{ 
+    book ? res.render('update-book',  { title: 'Update Book', book }) : res.sendStatus(404);
+  })
+  .catch(error => res.sendStatus(500, error));
 });
 
-/* GET books by id listing. */
-router.get('/:id', function(req, res, next) {
-  Book.findById(req.params.id).then( book =>{
-    console.log(book);
-    res.render('update-book',  { title: 'Update Book', book });
-  });
-});
-
-/* POST books by id listing. */
-router.post('/:id', function(req, res, next) {
+/* POST UPDATE books by id. */
+router.post('/:id', (req, res, next) =>{
   Book.findById(req.params.id)
   .then(book =>{
     return book.update(req.body);
   })
   .then(() =>{
-    res.redirect('/books/');
-  });
+    res.redirect('/books');
+  })
+  .catch(error => res.sendStatus(500, error));
 });
 
-/* POST books by id listing. */
-router.post('/:id/delete', function(req, res, next) {
+/* POST DELETE books by id. */
+router.post('/:id/delete', (req, res, next) =>{
   Book.findById(req.params.id)
   .then(book =>{
     return book.destroy();
   })
   .then(() =>{
-    res.redirect('/books/');
+    res.redirect('/books');
   });
 });
 
