24edacd3955830533710df76b6d0c97788da13fc
Warren Leyes <warren.leyes@gmail.com>
2018-10-11 11:26:51 -0700

    added pagination

--- routes/books.js
+++ routes/books.js
@@ -13,9 +13,7 @@ router.get('/', (req, res, next) => {
   Book.findAll({
     order: [["title", "ASC"]]
   })
-  .then( books =>{
-    res.render('index',  { title: 'All Books', books });
-  });
+  .then( books => res.render('index', { title: 'All Books', books }) )
 });
 
 router.post('/search', (req, res, next) => {
@@ -28,9 +26,7 @@ router.post('/search', (req, res, next) => {
       ]
     }
   })
-  .then( books =>{
-    res.render('index',  { title: 'Search Results', books });
-  });
+  .then( books => res.render('index',  { title: 'Search Results', books }) );
 })
 
 /* GET new books form listing. */
@@ -55,38 +51,30 @@ router.post('/new', (req, res, next) => {
 });
 
 /* GET READ books by id. */
-router.get('/:id', (req, res, next) =>{
+router.get('/:id', (req, res, next) => {
   Book.findById(req.params.id).then( book =>{ 
     book ? res.render('update-book',  { title: 'Update Book', book }) : (
       error = new Error('Page Not Found'),
       error.status=404,
       res.render('error',{error, title: `${error.status}-${error.message}`})
-);
+    );
   })
   .catch(error => res.sendStatus(500, error));
 });
 
 /* POST UPDATE books by id. */
-router.post('/:id', (req, res, next) =>{
+router.post('/:id', (req, res, next) => {
   Book.findById(req.params.id)
-  .then(book =>{
-    return book.update(req.body);
-  })
-  .then(() =>{
-    res.redirect('/books');
-  })
+  .then( book => book.update(req.body) )
+  .then( () => res.redirect('/books') )
   .catch(error => res.sendStatus(500, error));
 });
 
 /* POST DELETE books by id. */
 router.post('/:id/delete', (req, res, next) =>{
   Book.findById(req.params.id)
-  .then(book =>{
-    return book.destroy();
-  })
-  .then(() =>{
-    res.redirect('/books');
-  });
+  .then( book => book.destroy() )
+  .then( () => res.redirect('/books') );
 });
 
 module.exports = router;
